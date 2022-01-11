65b42be49d5498cc709df14e19fe4091e94666b4
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-10 23:13:06 -0500

    commit tree c86512e5b2cd9f265772637faadf9ccd4d0bc489

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 32;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(1, 2, 3, 4, 20);
